35d349d5042d92ee23f95d87f0750b9f312a2837
Himanshu Garg <himan.gargcoder@gmail.com>
2023-12-17 21:23:47 +0530

    indent fix

--- pages/Ask_Help.py
+++ pages/Ask_Help.py
@@ -26,27 +26,27 @@ i = 2
 while True:
     if user_message:
         st.session_state["messages"].append({"role": "user", "content": user_message})
-            try:
-                
-                client = OpenAI(api_key=st.secrets["AIOSH1"])
-                response = client.chat.completions.create(
-                  model="gpt-3.5-turbo",
-                  messages=st.session_state["messages"]
-                )
-                assistant_message = response.choices[0].message
-                print(assistant_message)
-                st.session_state["messages"].append({"role" : "assistant", "content":assistant_message.content}) #{"role": "assistant", "content": assistant_message})
-                st.chat_message("assistant").write(assistant_message.content)
-            except Exception as e:
-                st.chat_message("assistant").write("Sorry, an error occurred. Try again in a moment.")
-                st.write(e)
-                st.stop()
-            except openai.RateLimitError:
-                st.chat_message("assistant").write("LImit crossed")
-                st.stop()
-            time.sleep(5)
-            user_message = st.text_input("Your message", key=f"message{i}")
-            st.session_state["messages"].append({"role": "user", "content": user_message})
-            i+=2
-        else:
-            st.chat_message("assistant").write("Please add your OpenAI API key.")
+        try:
+            
+            client = OpenAI(api_key=st.secrets["AIOSH1"])
+            response = client.chat.completions.create(
+              model="gpt-3.5-turbo",
+              messages=st.session_state["messages"]
+            )
+            assistant_message = response.choices[0].message
+            print(assistant_message)
+            st.session_state["messages"].append({"role" : "assistant", "content":assistant_message.content}) #{"role": "assistant", "content": assistant_message})
+            st.chat_message("assistant").write(assistant_message.content)
+        except Exception as e:
+            st.chat_message("assistant").write("Sorry, an error occurred. Try again in a moment.")
+            st.write(e)
+            st.stop()
+        except openai.RateLimitError:
+            st.chat_message("assistant").write("LImit crossed")
+            st.stop()
+        time.sleep(5)
+        user_message = st.text_input("Your message", key=f"message{i}")
+        st.session_state["messages"].append({"role": "user", "content": user_message})
+        i+=2
+    else:
+        st.chat_message("assistant").write("Please add your OpenAI API key.")
